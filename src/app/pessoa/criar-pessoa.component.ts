390110685a9857ba1acbfd416cca15c4ce7c0ff6
felipenanes <felipenanes@gmail.com>
2020-10-01 17:11:42 -0300

    enviando novas validacoes para edicao e adicao

--- src/app/pessoa/criar-pessoa.component.ts
+++ src/app/pessoa/criar-pessoa.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Pessoa } from '../models/pessoa';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { Toast, ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pessoa',
@@ -13,13 +13,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class CriarPessoaComponent implements OnInit {
 
   submitted = false;
-  
+  sexOptions = [];
   model = new Pessoa('','','','','','','');
   criarPessoa: FormGroup;
 
   constructor(private http: HttpClient, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit(): void {
+      this.sexOptions = ['Male', 'Female', 'Undefined'];
       this.criarPessoa = new FormGroup({
         name: new FormControl(this.model.name, [
           Validators.required,
@@ -32,7 +33,8 @@ export class CriarPessoaComponent implements OnInit {
           Validators.required
         ]),
         cpf: new FormControl(this.model.cpf, [
-          Validators.required
+          Validators.required,
+          this.isValidCpf
         ]),
       });
   }
@@ -55,4 +57,58 @@ export class CriarPessoaComponent implements OnInit {
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
 
+  /**
+    * Valida se o CPF é valido. Deve-se ser informado o cpf sem máscara.
+   */
+  isValidCpf(control: AbstractControl): {[key: string]: any} | null {
+      const cpf = control.value;
+      if (cpf) {
+        let numbers, digits, sum, i, result, equalDigits;
+        equalDigits = 1;
+        if (cpf.length < 11) {
+        return null;
+        }
+
+        for (i = 0; i < cpf.length - 1; i++) {
+          if (cpf.charAt(i) !== cpf.charAt(i + 1)) {
+            equalDigits = 0;
+            break;
+          }
+        }
+
+        if (control.value && control.value.length < 11) {
+          return { 'cpfInvalid': true };
+        }
+        
+        if (!equalDigits) {
+          numbers = cpf.substring(0, 9);
+          digits = cpf.substring(9);
+          sum = 0;
+          for (i = 10; i > 1; i--) {
+            sum += numbers.charAt(10 - i) * i;
+          }
+
+          result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+          if (result !== Number(digits.charAt(0))) {
+            return { 'cpfNotValid': true };
+          }
+          numbers = cpf.substring(0, 10);
+          sum = 0;
+
+          for (i = 11; i > 1; i--) {
+            sum += numbers.charAt(11 - i) * i;
+          }
+          result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+          if (result !== Number(digits.charAt(1))) {
+            return { 'cpfNotValid': true };
+          }
+          return null;
+        } else {
+          return { 'cpfNotValid': true };
+        }
+    }
+    return null;
+  }
 }
--- src/app/pessoa/detalhe-pessoa.component.ts
+++ src/app/pessoa/detalhe-pessoa.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
@@ -14,9 +15,10 @@ import { PessoaService } from '../service/pessoa.service';
 export class DetalhePessoaComponent implements OnInit {
 
   submitted = false;
-
+  sexOptions = [];
   model = new Pessoa('','','','','','','');
   id: number;
+  criarPessoa: FormGroup;
 
   constructor(
     private http: HttpClient, 
@@ -26,6 +28,25 @@ export class DetalhePessoaComponent implements OnInit {
     private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.sexOptions = ['Male', 'Female', 'Undefined'];
+    
+    this.criarPessoa = new FormGroup({
+      name: new FormControl(this.model.name, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      email: new FormControl(this.model.email, [
+        Validators.email
+      ]),
+      birthDate: new FormControl(this.model.birthDate, [
+        Validators.required
+      ]),
+      cpf: new FormControl(this.model.cpf, [
+        Validators.required,
+        this.isValidCpf
+      ]),
+    });
+
     this.id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.pessoaService.detail(this.id).subscribe(
       (response) => {
@@ -58,4 +79,58 @@ export class DetalhePessoaComponent implements OnInit {
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
 
+    /**
+    * Valida se o CPF é valido. Deve-se ser informado o cpf sem máscara.
+   */
+  isValidCpf(control: AbstractControl): {[key: string]: any} | null {
+    const cpf = control.value;
+    if (cpf) {
+      let numbers, digits, sum, i, result, equalDigits;
+      equalDigits = 1;
+      if (cpf.length < 11) {
+      return null;
+      }
+
+      for (i = 0; i < cpf.length - 1; i++) {
+        if (cpf.charAt(i) !== cpf.charAt(i + 1)) {
+          equalDigits = 0;
+          break;
+        }
+      }
+
+      if (control.value && control.value.length < 11) {
+        return { 'cpfInvalid': true };
+      }
+      
+      if (!equalDigits) {
+        numbers = cpf.substring(0, 9);
+        digits = cpf.substring(9);
+        sum = 0;
+        for (i = 10; i > 1; i--) {
+          sum += numbers.charAt(10 - i) * i;
+        }
+
+        result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+        if (result !== Number(digits.charAt(0))) {
+          return { 'cpfNotValid': true };
+        }
+        numbers = cpf.substring(0, 10);
+        sum = 0;
+
+        for (i = 11; i > 1; i--) {
+          sum += numbers.charAt(11 - i) * i;
+        }
+        result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+        if (result !== Number(digits.charAt(1))) {
+          return { 'cpfNotValid': true };
+        }
+        return null;
+      } else {
+        return { 'cpfNotValid': true };
+      }
+  }
+  return null;
+}
 }
